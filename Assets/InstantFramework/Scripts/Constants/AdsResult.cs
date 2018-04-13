5e7086828ab10485a5e7cc14f9c22d35c4448a45
Faraz Ahmed <arcifus@gmail.com>
2018-04-13 14:18:52 +0500

    Add ads service

---
+++ Assets/InstantFramework/Scripts/Constants/AdsResult.cs
@@ -0,0 +1,21 @@
+/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2017 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+/// 
+/// @author Mubeen Iqbal <mubeen@turbolabz.com>
+/// @company Turbo Labz <http://turbolabz.com>
+/// @date 2017-12-08 22:47:21 UTC+05:00
+/// 
+/// @description
+/// [add_description_here]
+
+namespace TurboLabz.InstantFramework
+{
+    public enum AdsResult
+    {
+        FINISHED,
+        SKIPPED,
+        FAILED
+    }
+}
---
+++ Assets/InstantFramework/Scripts/Services/UnityAds/UnityAdsService.cs
@@ -0,0 +1,48 @@
+/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2017 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+/// 
+/// @author Mubeen Iqbal <mubeen@turbolabz.com>
+/// @company Turbo Labz <http://turbolabz.com>
+/// @date 2017-12-08 16:09:30 UTC+05:00
+/// 
+/// @description
+/// [add_description_here]
+
+using UnityEngine.Advertisements;
+
+using strange.extensions.promise.api;
+
+namespace TurboLabz.InstantFramework
+{
+    public class UnityAdsService : IAdsService
+    {
+        public bool isRewardedAdAvailable
+        {
+            get
+            {
+                return Advertisement.IsReady(UnityAdsPlacementId.REWARDED_VIDEO);
+            }
+        }
+
+        public bool isAdAvailable 
+        { 
+            get
+            {
+                return Advertisement.IsReady();
+            }
+        }
+
+        public IPromise<AdsResult> ShowRewardedAd()
+        {
+            return new UnityAdsShowRewardedAdRequest().Send();
+        }
+
+
+        public void ShowAd()
+        {
+            Advertisement.Show();
+        }
+    }
+}
