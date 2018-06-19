30e561f3da615a0e2eed6f72a6b98fc8bed6821f
Faraz Ahmed <arcifus@gmail.com>
2018-06-19 20:55:21 +0500

    Enable multiplayer game (wip)

---
+++ Assets/Game/Scripts/Common/Controllers/MatchFindingCommand.cs
@@ -0,0 +1,27 @@
+/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2017 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+///
+/// @author Faraz Ahmed <faraz@turbolabz.com>
+/// @company Turbo Labz <http://turbolabz.com>
+/// @date 2017-01-17 15:42:00 UTC+05:00
+///
+/// @description
+/// [add_description_here]
+
+using strange.extensions.command.impl;
+
+using TurboLabz.InstantFramework;
+using TurboLabz.Chess;
+
+namespace TurboLabz.InstantFramework
+{
+    public class MatchFindingCommand : Command
+    {
+        public override void Execute()
+        {
+            
+        }
+    }
+}
