f9a2d9afab3de61b127f372e741c1af2930c4b83
nkhawaja1 <noor_khawaja@hotmail.com>
2018-07-10 17:10:10 +0500

    Save player inventory WIP

---
+++ Assets/InstantFramework/Scripts/Controllers/SavePlayerInventoryCommand.cs
@@ -0,0 +1,42 @@
+
+
+using strange.extensions.command.impl;
+using UnityEngine;
+
+namespace TurboLabz.InstantFramework
+{
+    public class SavePlayerInventoryCommand : Command
+    {
+        // Models
+        [Inject] public IPlayerModel playerModel { get; set; }
+
+        // Services
+        [Inject] public IBackendService backendService { get; set; }
+
+        // Signals
+        [Inject] public BackendErrorSignal backendErrorSignal { get; set; }
+
+        public override void Execute()
+        {
+            Retain();
+
+            // TODO: This is hardcoded to only handle skins. Make this generic for new item types
+            TLUtils.LogUtil.Log("SavePlayerInventoryCommand()  " + playerModel.activeSkinId);
+            backendService.UpdateActiveInventory(playerModel.activeSkinId).Then(OnComplete);
+        }
+
+        private void OnComplete(BackendResult result)
+        {
+            TLUtils.LogUtil.Log("SavePlayerInventoryCommand::OnComplete()", "cyan");
+
+            if (result != BackendResult.SUCCESS && result != BackendResult.CANCELED)
+            {
+                backendErrorSignal.Dispatch(result);
+
+                TLUtils.LogUtil.Log("SavePlayerInventoryCommand::OnComplete() success!", "cyan");
+            }
+
+            Release();
+        }
+    }
+}
