5f9ea94e1d381ca48aeef9ec9b22e36a0dbb6ca5
Faraz Ahmed <arcifus@gmail.com>
2018-08-10 16:15:43 +0500

    Friends wip

---
+++ Assets/Game/Scripts/Friends/Views/FriendBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TurboLabz.InstantFramework
+{
+    public class FriendBar : MonoBehaviour
+    {
+        public Image avatarImage;
+        public Text profileNameLabel;
+        public Text eloScoreLabel;
+        public GameObject timer;
+        public Text timerLabel;
+        public Text statusLabel;
+        public Button viewProfileButton;
+        public Button playButton;
+    }
+}
