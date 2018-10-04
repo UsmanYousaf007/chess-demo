9e96b4262747a33f928e783f1ca5b6b15f0946fc
Faraz Ahmed <arcifus@gmail.com>
2018-10-04 14:00:17 +0500

    chat wip

---
+++ Assets/InstantFramework/Scripts/Views/ChatBubble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ChatBubble : MonoBehaviour 
+{
+    public TextMeshProUGUI text;
+    public RectTransform bg;
+
+    const float xOffset = 50f;
+    const float yOffset = 22f;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        text.ForceMeshUpdate();
+        Vector3 textBoundsSize = text.textBounds.size;
+        bg.sizeDelta = new Vector2(textBoundsSize.x + xOffset, textBoundsSize.y + yOffset);
+        GetComponent<RectTransform>().sizeDelta = bg.sizeDelta;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+
+	}
+}
