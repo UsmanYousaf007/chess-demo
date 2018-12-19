cfd171141945d11839b27ef1847aafbd00c00385
nkhawaja1 <noor_khawaja@hotmail.com>
2018-12-19 18:02:45 +0500

    Long match timer to 00:00:00 format

--- Assets/Game/Scripts/Multiplayer/Views/GameView_Clock.cs
+++ Assets/Game/Scripts/Multiplayer/Views/GameView_Clock.cs
@@ -55,66 +55,18 @@ namespace TurboLabz.Multiplayer
         {
             // TODO: localize clock
 
-            if (timer.TotalHours > 1)
+            long seconds = timer.Seconds;
+            long minutes = timer.Minutes;
+            long hours = timer.Hours;
+
+            if (timer.Days > 0)
             {
-                int days = timer.Days;
-                int hours = timer.Hours;
-                int minutes = timer.Minutes;
+                hours += timer.Days * 24;
+            }
 
-                // Day calculation
-                if (timer.Hours == 23 && (timer.Minutes > 0 || timer.Seconds > 0))
-                {
-                    days++;
-                    hours = 0;
-                    minutes = 0;
-                }
-                // Hours calculation
-                else if (timer.Minutes == 59 && timer.Seconds > 0)
-                {
-                    hours++;
-                    minutes = 0;
-                }
-                else if (days > 0 && (timer.Minutes > 0 || timer.Seconds > 0))
-                {
-                    hours++;
-                    minutes = 0;
-                }
-                // Minutes calculation
-                else if (timer.Seconds > 0)
-                {
-                    minutes++;
-                }
-
-                StringBuilder sb = new StringBuilder();
-                if (days > 0)
-                {
-                    sb.Append(days);
-                    sb.Append("d");
-                }
-
-                if (hours > 0)
-                {
-                    if (days > 0)
-                    {
-                        sb.Append(" ");
-
-                    }
-                    sb.Append(hours);
-                    sb.Append("h");
-                }
-
-                if (minutes > 0 && (days == 0))
-                {
-                    if (hours > 0)
-                    {
-                        sb.Append(" ");
-
-                    }
-                    sb.Append(minutes);
-                    sb.Append("m");
-                }
-
-                return sb.ToString();
+            if (timer.TotalHours >= 1)
+            {
+                return string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
             }
 
             // else show 00:00 format
--- Assets/Snippets/TimerTest.cs
+++ Assets/Snippets/TimerTest.cs
@@ -12,17 +12,17 @@ public class TimerTest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        TimeSpan t1 = new TimeSpan(2, 0, 0, 0);        // 2d
-        TimeSpan t2 = new TimeSpan(3, 23, 59, 59);    // 2d
-        TimeSpan t3 = new TimeSpan(1, 23, 0, 0);    // 1d 23h
-        TimeSpan t4 = new TimeSpan(1, 22, 59, 59);    // 1d 23h
-        TimeSpan t5 = new TimeSpan(1, 0, 59, 59);    // 1d 1h
-        TimeSpan t6 = new TimeSpan(1, 0, 0, 59);    // 1d 1h
-        TimeSpan t7 = new TimeSpan(1, 0, 0, 0);        // 1d
-        TimeSpan t8 = new TimeSpan(23, 59, 59);        // 1d
-        TimeSpan t9 = new TimeSpan(1, 1, 0);        // 1h 1m
-        TimeSpan t10 = new TimeSpan(1, 0, 1);        // 1h 1m
-        TimeSpan t11 = new TimeSpan(1, 0, 0);        // 1h
+        TimeSpan t1 = new TimeSpan(2, 0, 0, 0);        // 48:00:00
+        TimeSpan t2 = new TimeSpan(3, 23, 59, 59);    // 95:59:59
+        TimeSpan t3 = new TimeSpan(1, 23, 0, 0);    // 47:00:00
+        TimeSpan t4 = new TimeSpan(1, 22, 59, 59);    // 46:59:59
+        TimeSpan t5 = new TimeSpan(1, 22, 59, 0);    // 46:59:00
+        TimeSpan t6 = new TimeSpan(1, 0, 0, 59);    // 24:00:59
+        TimeSpan t7 = new TimeSpan(1, 0, 0, 0);        // 24:00:00
+        TimeSpan t8 = new TimeSpan(23, 59, 59);        // 23:59:59
+        TimeSpan t9 = new TimeSpan(1, 1, 0);        // 01:01:00
+        TimeSpan t10 = new TimeSpan(1, 0, 1);        // 01:00:01
+        TimeSpan t11 = new TimeSpan(1, 0, 0);        // 01:00:00
         TimeSpan t12 = new TimeSpan(0, 59, 59);        // 59:59
 
         Debug.Log("t1=" + FormatTimer(t1));
@@ -38,8 +38,8 @@ public class TimerTest : MonoBehaviour {
         Debug.Log("t11=" + FormatTimer(t11));
         Debug.Log("t12=" + FormatTimer(t12));
 
-        ticker = new TimeSpan(0, 2, 0, 10);
 
+        ticker = new TimeSpan( 1, 0, 10);
         StartCoroutine(Tick());
     }
 
@@ -63,66 +63,18 @@ public class TimerTest : MonoBehaviour {
     {
         // TODO: localize clock
 
-        if (timer.TotalHours > 1)
+        long seconds = timer.Seconds;
+        long minutes = timer.Minutes;
+        long hours = timer.Hours;
+
+        if (timer.Days > 0)
         {
-            int days = timer.Days;
-            int hours = timer.Hours;
-            int minutes = timer.Minutes;
+            hours += timer.Days * 24;
+        }
 
-            // Day calculation
-            if (timer.Hours == 23 && (timer.Minutes > 0 || timer.Seconds > 0))
-            {
-                days++;
-                hours = 0;
-                minutes = 0;
-            }
-            // Hours calculation
-            else if (timer.Minutes == 59 && timer.Seconds > 0)
-            {
-                hours++;
-                minutes = 0;
-            }
-            else if (days > 0 && (timer.Minutes > 0 || timer.Seconds > 0))
-            {
-                hours++;
-                minutes = 0;
-            }
-            // Minutes calculation
-            else if (timer.Seconds > 0)
-            {
-                minutes++;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            if (days > 0)
-            {
-                sb.Append(days);
-                sb.Append("d");
-            }
-
-            if (hours > 0)
-            {
-                if (days > 0)
-                {
-                    sb.Append(" ");
-
-                }
-                sb.Append(hours);
-                sb.Append("h");
-            }
-
-            if (minutes > 0 && (days == 0))
-            {
-                if (hours > 0)
-                {
-                    sb.Append(" ");
-
-                }
-                sb.Append(minutes);
-                sb.Append("m");
-            }
-
-            return sb.ToString();
+        if (timer.TotalHours >= 1)
+        {
+            return string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
         }
 
         // else show 00:00 format
