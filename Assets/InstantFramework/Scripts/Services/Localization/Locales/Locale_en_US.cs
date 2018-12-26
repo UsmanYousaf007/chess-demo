b9c694d5835ec91f6efc6cd0a4a619f269db433d
nkhawaja1 <noor_khawaja@hotmail.com>
2018-12-26 20:08:50 +0500

    Friends page FB login dots position update

--- Assets/InstantFramework/Scripts/Services/Localization/Locales/Locale_en_US.cs
+++ Assets/InstantFramework/Scripts/Services/Localization/Locales/Locale_en_US.cs
@@ -182,7 +182,7 @@ namespace TurboLabz.InstantFramework
 				{ LocalizationKey.FRIENDS_DRAWS_LABEL, "Draws" },
 				{ LocalizationKey.FRIENDS_TOTAL_GAMES_LABEL, "Total Games: " },
 				{ LocalizationKey.FRIENDS_BLOCK_LABEL, "Block User" },
-                { LocalizationKey.FRIENDS_FACEBOOK_CONNECT_TEXT, "Log in to Facebook to play\nwith Friends & Community" },
+                { LocalizationKey.FRIENDS_FACEBOOK_CONNECT_TEXT, "Log in to play with Friends & Community" },
                 { LocalizationKey.FRIENDS_FACEBOOK_LOGIN_BUTTON_TEXT, "Login" },
                 { LocalizationKey.FRIENDS_WAITING_FOR_PLAYERS, "Waiting for players ...\n" },
 
