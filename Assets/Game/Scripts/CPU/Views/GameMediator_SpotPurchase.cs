82968956bac32b42f106c2afb4d7d65239ef86c8
Faraz Ahmed <arcifus@gmail.com>
2019-01-25 14:12:33 +0500

    Misc fixes

--- Assets/Game/Scripts/CPU/Views/GameMediator_SpotPurchase.cs
+++ Assets/Game/Scripts/CPU/Views/GameMediator_SpotPurchase.cs
@@ -16,5 +16,23 @@ namespace TurboLabz.CPU
         {
             loadSpotPurchaseSignal.Dispatch(activeSection);
         }
+
+        [ListensTo(typeof(NavigatorShowViewSignal))]
+        public void OnShowSpotPurchase(NavigatorViewId viewId)
+        {
+            if (viewId == NavigatorViewId.SPOT_PURCHASE_DLG)
+            {
+                view.chessboardBlocker.SetActive(true);
+            }
+        }
+
+        [ListensTo(typeof(NavigatorHideViewSignal))]
+        public void OhHideSpotPurchase(NavigatorViewId viewId)
+        {
+            if (viewId == NavigatorViewId.SPOT_PURCHASE_DLG)
+            {
+                view.chessboardBlocker.SetActive(false);
+            }
+        }
     }
 }
--- Assets/Game/Scripts/Multiplayer/Views/GameMediator_SpotPurchase.cs
+++ Assets/Game/Scripts/Multiplayer/Views/GameMediator_SpotPurchase.cs
@@ -16,5 +16,23 @@ namespace TurboLabz.Multiplayer
         {
             loadSpotPurchaseSignal.Dispatch(activeSection);
         }
+
+        [ListensTo(typeof(NavigatorShowViewSignal))]
+        public void OnShowSpotPurchase(NavigatorViewId viewId)
+        {
+            if (viewId == NavigatorViewId.SPOT_PURCHASE_DLG)
+            {
+                view.chessboardBlocker.SetActive(true);
+            }
+        }
+
+        [ListensTo(typeof(NavigatorHideViewSignal))]
+        public void OhHideSpotPurchase(NavigatorViewId viewId)
+        {
+            if (viewId == NavigatorViewId.SPOT_PURCHASE_DLG)
+            {
+                view.chessboardBlocker.SetActive(false);
+            }
+        }
     }
 }
