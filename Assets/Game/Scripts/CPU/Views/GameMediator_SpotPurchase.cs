18de71ee9b7a4688cc9dc90052905985a389a5d4
Faraz Ahmed <arcifus@gmail.com>
2019-01-25 14:17:26 +0500

    Misc fixes

--- Assets/Game/Scripts/CPU/Views/GameMediator_SpotPurchase.cs
+++ Assets/Game/Scripts/CPU/Views/GameMediator_SpotPurchase.cs
@@ -1,4 +1,4 @@
-﻿using TurboLabz.InstantFramework;
+using TurboLabz.InstantFramework;
 using TurboLabz.InstantGame;
 
 namespace TurboLabz.CPU
@@ -6,6 +6,7 @@ namespace TurboLabz.CPU
     public partial class GameMediator
     {
         [Inject] public LoadSpotPurchaseSignal loadSpotPurchaseSignal { get; set; }
+        [Inject] public UpdatePlayerInventorySignal updatePlayerInventorySignal { get; set; }
 
         public void OnRegisterSpotPurchase()
         {
@@ -15,6 +16,14 @@ namespace TurboLabz.CPU
         void OnOpenSpotPurchase(SpotPurchaseView.PowerUpSections activeSection)
         {
             loadSpotPurchaseSignal.Dispatch(activeSection);
+        }
+
+        [ListensTo(typeof(UpdatePlayerInventorySignal))]
+        public void OnUpdatePlayerInventory(PlayerInventoryVO vo)
+        {
+            view.UpdateHintCount(vo.hintCount);
+            view.UpdateSafeMoveCount(vo.safeMoveCount);
+            view.UpdateHindsightCount(vo.hindsightCount);
         }
 
         [ListensTo(typeof(NavigatorShowViewSignal))]
--- Assets/Game/Scripts/Multiplayer/Views/GameMediator_SpotPurchase.cs
+++ Assets/Game/Scripts/Multiplayer/Views/GameMediator_SpotPurchase.cs
@@ -1,4 +1,4 @@
-﻿using TurboLabz.InstantFramework;
+using TurboLabz.InstantFramework;
 using TurboLabz.InstantGame;
 
 namespace TurboLabz.Multiplayer
@@ -6,6 +6,7 @@ namespace TurboLabz.Multiplayer
     public partial class GameMediator
     {
         [Inject] public LoadSpotPurchaseSignal loadSpotPurchaseSignal { get; set; }
+        [Inject] public UpdatePlayerInventorySignal updatePlayerInventorySignal { get; set; }
 
         public void OnRegisterSpotPurchase()
         {
@@ -15,6 +16,14 @@ namespace TurboLabz.Multiplayer
         void OnOpenSpotPurchase(SpotPurchaseView.PowerUpSections activeSection)
         {
             loadSpotPurchaseSignal.Dispatch(activeSection);
+        }
+
+        [ListensTo(typeof(UpdatePlayerInventorySignal))]
+        public void OnUpdatePlayerInventory(PlayerInventoryVO vo)
+        {
+            view.UpdateHintCount(vo.hintCount);
+            view.UpdateSafeMoveCount(vo.safeMoveCount);
+            view.UpdateHindsightCount(vo.hindsightCount);
         }
 
         [ListensTo(typeof(NavigatorShowViewSignal))]
