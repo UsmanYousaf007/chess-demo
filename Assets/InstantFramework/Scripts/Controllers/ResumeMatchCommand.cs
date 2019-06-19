fc37103bc80ecf16b7fe5129b592f247bac27e86
nkhawaja1 <noor_khawaja@hotmail.com>
2019-06-19 16:02:18 +0500

    Resume Match Command

---
+++ Assets/InstantFramework/Scripts/Controllers/ResumeMatchCommand.cs
@@ -0,0 +1,42 @@
+﻿/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2016 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+using UnityEngine;
+using strange.extensions.command.impl;
+using TurboLabz.TLUtils;
+using System;
+
+namespace TurboLabz.InstantFramework
+{
+    public class ResumeMatchCommand : Command
+    {
+        // Dispatch signals
+        [Inject] public StartGameSignal startGameSignal { get; set; }
+        [Inject] public BackendErrorSignal backendErrorSignal { get; set; }
+
+        // services
+        [Inject] public IBackendService backendService { get; set; }
+
+        public override void Execute()
+        {
+            Retain();
+            backendService.ResumeMatchData().Then(OnResumeMatchData);
+        }
+
+        public void OnResumeMatchData(BackendResult result)
+        {
+            if (result == BackendResult.SUCCESS)
+            {
+                startGameSignal.Dispatch();
+            }
+            else 
+            {
+                backendErrorSignal.Dispatch(result);
+            }
+
+            Release();
+        }
+    }
+}
