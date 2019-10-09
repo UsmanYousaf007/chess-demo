d9b136136ab413714f03e442a103b5ae84f4c48c
UsmanYousaf007 <usman.yousaf@turbolabz.com>
2019-10-09 16:43:19 +0500

    Update AppsFlyer Remove Tapjoy SDK

--- Assets/Plugins/iOS/AppsFlyerDelegate.mm
+++ Assets/Plugins/iOS/AppsFlyerDelegate.mm
@@ -21,7 +21,10 @@
 
 - (instancetype)init
 {
-    NSLog(@"initializing AppsFlyerDelegate");
+    if([AppsFlyerTracker sharedTracker].isDebug){
+        NSLog(@"initializing AppsFlyerDelegate");
+    }
+    
     self = [super init];
     if (self) {
     }
@@ -33,26 +36,34 @@
 - (void) onConversionDataReceived:(NSDictionary*) installData {
     
     NSString *conversionDataResult = [self getJsonStringFromDictionary:installData];
-    NSLog (@"AppsFlyerDelegate onConversionDataReceived = %@", conversionDataResult);
+    if([AppsFlyerTracker sharedTracker].isDebug){
+        NSLog (@"AppsFlyerDelegate onConversionDataReceived = %@", conversionDataResult);
+    }
     UnitySendMessage(UNITY_SENDMESSAGE_CALLBACK_MANAGER, UNITY_SENDMESSAGE_CALLBACK_CONVERSION, [conversionDataResult UTF8String]);
 }
 
 - (void) onConversionDataRequestFailure:(NSError *)error {
     
     NSString *errDesc = [error localizedDescription];
-    NSLog (@"AppsFlyerDelegate onConversionDataRequestFailure = %@", errDesc);
+    if([AppsFlyerTracker sharedTracker].isDebug){
+        NSLog (@"AppsFlyerDelegate onConversionDataRequestFailure = %@", errDesc);
+    }
     UnitySendMessage(UNITY_SENDMESSAGE_CALLBACK_MANAGER, UNITY_SENDMESSAGE_CALLBACK_CONVERSION_ERROR, [errDesc UTF8String]);
 }
 
 - (void) onAppOpenAttribution:(NSDictionary*) attributionData {
     NSString *attrData = [self getJsonStringFromDictionary:attributionData];
-    NSLog (@"AppsFlyerDelegate onAppOpenAttribution = %@", attrData);
+    if([AppsFlyerTracker sharedTracker].isDebug){
+        NSLog (@"AppsFlyerDelegate onAppOpenAttribution = %@", attrData);
+    }
     UnitySendMessage(UNITY_SENDMESSAGE_CALLBACK_MANAGER, UNITY_SENDMESSAGE_CALLBACK_RETARGETTING, [attrData UTF8String]);
 }
 
 - (void) onAppOpenAttributionFailure:(NSError *)error{
     NSString *errDesc = [error localizedDescription];
-    NSLog (@"AppsFlyerDelegate onAppOpenAttributionFailure = %@", errDesc);
+    if([AppsFlyerTracker sharedTracker].isDebug){
+        NSLog (@"AppsFlyerDelegate onAppOpenAttributionFailure = %@", errDesc);
+    }
     UnitySendMessage(UNITY_SENDMESSAGE_CALLBACK_MANAGER, UNITY_SENDMESSAGE_CALLBACK_RETARGETTING_ERROR, [errDesc UTF8String]);
     
 }
@@ -64,15 +75,17 @@
                                                          error:&error];
     
     if (!jsonData) {
-        NSLog(@"JSON error: %@", error);
         return nil;
     } else {
         
         NSString *JSONString = [[NSString alloc] initWithBytes:[jsonData bytes] length:[jsonData length] encoding:NSUTF8StringEncoding];
-        NSLog(@"JSON OUTPUT: %@", JSONString);
+        if([AppsFlyerTracker sharedTracker].isDebug){
+            NSLog(@"JSON OUTPUT: %@", JSONString);
+        }
         return JSONString;
     }
 }
 
 
 @end
+
