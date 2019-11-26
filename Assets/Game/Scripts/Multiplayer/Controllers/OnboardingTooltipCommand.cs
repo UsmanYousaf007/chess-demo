552ffb56f598453d3247c7b7528498bc37e6345b
Sami Atiq <sami.atiq@turbolabz.com>
2019-11-26 12:31:39 +0500

    onboarding tooltip bug fix

--- Assets/Game/Scripts/Multiplayer/Controllers/OnboardingTooltipCommand.cs
+++ Assets/Game/Scripts/Multiplayer/Controllers/OnboardingTooltipCommand.cs
@@ -20,29 +20,25 @@ namespace TurboLabz.Multiplayer
         public static int oldOpponentScore;
         public static int oldPlayerScore;
 
-        private const int POWERUP_USE_LIMIT = 1;
-        private const int POWERUP_TRAINING_DAYS_LIMIT = 30;
-
         public override void Execute()
         {
             if (!preferencesModel.isCoachTooltipShown
-               && moveVo.opponentScore > 0
                && moveVo.opponentScore > oldOpponentScore)
             {
-                oldOpponentScore = moveVo.opponentScore;
                 showCoachOnboardingTooltipSignal.Dispatch(true);
                 showStrengthOnboardingTooltipSignal.Dispatch(false);
                 preferencesModel.isCoachTooltipShown = true;
             }
             else if (!preferencesModel.isStrengthTooltipShown
-                && moveVo.playerScore > 0
                 && moveVo.playerScore > oldPlayerScore)
             {
-                oldPlayerScore = moveVo.playerScore;
                 showStrengthOnboardingTooltipSignal.Dispatch(true);
                 showCoachOnboardingTooltipSignal.Dispatch(false);
                 preferencesModel.isStrengthTooltipShown = true;
             }
+
+            oldOpponentScore = moveVo.opponentScore;
+            oldPlayerScore = moveVo.playerScore;
         }
     }
 }
