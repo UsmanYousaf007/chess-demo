1bcbf2cce871d813ae4427bc490294d0059b9183
Sami Atiq <sami.atiq@turbolabz.com>
2019-11-27 17:33:48 +0500

    fixed crash on relauching helpshift

--- Assets/InstantFramework/Scripts/Services/HelpShift/HelpShiftService.cs
+++ Assets/InstantFramework/Scripts/Services/HelpShift/HelpShiftService.cs
@@ -10,39 +10,36 @@ namespace TurboLabz.InstantFramework
         [Inject] public IPlayerModel playerModel { get; set; }
 
         private HelpshiftSdk helpshift;
-        private Dictionary<string, object> configMap = new Dictionary<string, object>();
 
         public void Init()
         {
 #if !UNITY_EDITOR
             helpshift = HelpshiftSdk.getInstance();
-            helpshift.install(Settings.HELPSHIFT_API_KEY, Settings.HELPSHIFT_DOMAIN_NAME, Settings.HELPSHIFT_APP_ID, configMap);
+            helpshift.install(Settings.HELPSHIFT_API_KEY, Settings.HELPSHIFT_DOMAIN_NAME, Settings.HELPSHIFT_APP_ID);
 #endif
         }
 
         public void ShowConversation()
         {
 #if !UNITY_EDITOR
-            helpshift.showConversation(configMap);
+            helpshift.showConversation();
 #endif
         }
 
         public void ShowFAQ()
         {
 #if !UNITY_EDITOR
-            var customMetaDeta = new Dictionary<string, object>();
-            SetupConfigMap();
-            customMetaDeta.Add(HelpshiftSdk.HSCUSTOMMETADATAKEY, configMap);
-            helpshift.showFAQs(customMetaDeta);
-#endif
-        }
-
-        private void SetupConfigMap()
-        {
+            var configMap = new Dictionary<string, object>();
             configMap.Add("DisplayName", playerModel.name);
             configMap.Add("EditedName", playerModel.editedName);
             configMap.Add("PlayerTag", playerModel.tag);
             configMap.Add("IsPremium", playerModel.isPremium);
+
+            var customMetaDeta = new Dictionary<string, object>();
+            customMetaDeta.Add(HelpshiftSdk.HSCUSTOMMETADATAKEY, configMap);
+
+            helpshift.showFAQs(customMetaDeta);
+#endif
         }
     }
 }
