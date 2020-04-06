d574c3f238301c51f3c9c5204eb3dcd671186930
Maryam Rasheed <maryam.rasheed@turbolabz.com>
2020-04-06 18:59:28 +0500

    no message

---
+++ Assets/InstantFramework/Scripts/Controllers/NavigatorStates/NSLimitReachedDlg.cs
@@ -0,0 +1,29 @@
+﻿namespace TurboLabz.InstantFramework
+{
+    public class NSLimitReachedDlg : NS
+    {
+        public override void RenderDisplayOnEnter()
+        {
+            ShowDialog(NavigatorViewId.CREATE_MATCH_LIMIT_REACHED_DIALOG);
+        }
+
+        public override NS HandleEvent(NavigatorEvent evt)
+        {
+            NavigatorViewId viewId = CameFrom(NavigatorViewId.LOBBY, NavigatorViewId.FRIENDS);
+
+            if (evt == NavigatorEvent.ESCAPE)
+            {
+                if (viewId == NavigatorViewId.LOBBY)
+                {
+                    return new NSLobby();
+                }
+                else if (viewId == NavigatorViewId.FRIENDS)
+                {
+                    return new NSFriends();
+                }
+            }
+
+            return null;
+        }
+    }
+}
