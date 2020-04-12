37e9dba6ecb07b62b39195738bab23a79123f45e
Noor Khawaja <noor.khawaja@turbolabz.com>
2020-04-13 03:12:37 +0500

    Azure Functions wip

--- Assets/SocialEdge/Requests/AzureFunctions.cs
+++ Assets/SocialEdge/Requests/AzureFunctions.cs
@@ -8,100 +8,160 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class AzureFunctionResponse
+
+namespace SocialEdge.Requests
 {
-    public bool isSuccess;
-    public long responseTime;
-}
-
-public class AzureFunctionRequest
-{
-    // Todo: Prefab for settings
-    private const string AZURE_TITLE_URL = "https://chessstars.azurewebsites.net/api/";
-
-    class RoutineRunnerBehavior : MonoBehaviour {}
-    RoutineRunnerBehavior monoBehavior;
-    WWWForm form;
-    DateTime requestTimestamp;
-    long responseTime;
-    Action<AzureFunctionResponse> successCB;
-    Action<AzureFunctionResponse> failureCB;
-    AzureFunctionResponse response;
-    string functionName;
-    string titleURL;
-    string postURL;
-
-    public enum Functions
+    public class AzureRoutineRunnerBehavior : MonoBehaviour { }
+    public class AzureRoutineRunner
     {
-        ProductReviewTest           // Test
-    };
+        private const string GAME_OBJECT_NAME = "AzureRoutineRunnerBehavior";
+        public MonoBehaviour monoBehavior;
 
-    public AzureFunctionRequest()
-    {
-        form = new WWWForm();
-        response = new AzureFunctionResponse();
-    }
-
-    public AzureFunctionRequest SetFunction(Functions fn)
-    {
-        functionName = fn.ToString();
-        postURL = titleURL + functionName;
-        return this;
-    }
-
-    public AzureFunctionRequest AddField(string fieldName, int intValue)
-    {
-        form.AddField(fieldName, intValue);
-        return this;
-    }
-
-    public AzureFunctionRequest AddField(string fieldName, string stringValue)
-    {
-        form.AddField(fieldName, stringValue);
-        return this;
-    }
-
-    public AzureFunctionRequest SetSuccessCallback(Action<AzureFunctionResponse> responseCallback)
-    {
-        successCB = responseCallback;
-        return this;
-    }
-
-    public AzureFunctionRequest SetFailureCallback(Action<AzureFunctionResponse> responseCallback)
-    {
-        failureCB = responseCallback;
-        return this;
-    }
-
-    public void Send()
-    {
-        monoBehavior.StartCoroutine(ExecuteAzureWebFunction());
-    }
-
-    private IEnumerator ExecuteAzureWebFunction()
-    {
-        requestTimestamp = DateTime.UtcNow;
-        using (UnityWebRequest www = UnityWebRequest.Post(postURL, form))
+        public AzureRoutineRunner()
         {
-            yield return www.SendWebRequest();
+            GameObject go = GameObject.Find(GAME_OBJECT_NAME);
 
-            responseTime = (long)((DateTime.UtcNow - requestTimestamp).TotalMilliseconds);
-            response.responseTime = responseTime;
-
-            if (www.isNetworkError || www.isHttpError)
+            if (go == null)
             {
-                if (failureCB != null)
-                {
-                    response.isSuccess = false;
-             
-                    failureCB(response);
-                }
+                go = new GameObject(GAME_OBJECT_NAME);
             }
-            else if (successCB != null)
+
+            monoBehavior = go.GetComponent<AzureRoutineRunnerBehavior>();
+
+            if (monoBehavior == null)
             {
-                response.isSuccess = true;
-                successCB(response);
+                monoBehavior = go.AddComponent<AzureRoutineRunnerBehavior>();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Azure function request result response
+    /// </summary>
+    public class AzureFunctionResponse
+    {
+        public bool isSuccess;
+        public long responseTime;
+    }
+
+    /// <summary>
+    /// Azure function request
+    /// </summary>
+    public class AzureFunctionRequest
+    {
+        // Todo: Prefab for settings
+        private const string AZURE_TITLE_URL = "https://chessstars.azurewebsites.net/api/";
+
+        protected AzureRoutineRunner routineRunner;
+        Coroutine coroutineFunction;
+        WWWForm form;
+        long responseTime;
+        DateTime requestTimestamp;
+        string functionName;
+        string titleURL;
+        string postURL;
+        AzureFunctionResponse response;
+        Action<AzureFunctionResponse> successCB;
+        Action<AzureFunctionResponse> failureCB;
+
+        public enum Functions
+        {
+            ProductReviewTest,           // Test
+            Test
+        };
+
+        /// <summary>
+        /// Azure function constructor
+        /// </summary>
+        public AzureFunctionRequest()
+        {
+            titleURL = AZURE_TITLE_URL; // place holder
+            form = new WWWForm();
+            response = new AzureFunctionResponse();
+            routineRunner = new AzureRoutineRunner();
+        }
+
+        /// <summary>
+        /// Set the function to call
+        /// </summary>
+        public AzureFunctionRequest SetFunction(Functions fn)
+        {
+            functionName = fn.ToString();
+            postURL = titleURL + functionName;
+            return this;
+        }
+
+        /// <summary>
+        /// Add an integer function parameter field
+        /// </summary>
+        public AzureFunctionRequest AddField(string fieldName, int intValue)
+        {
+            form.AddField(fieldName, intValue);
+            return this;
+        }
+
+        /// <summary>
+        /// Add a string function parameter field
+        /// </summary>
+        public AzureFunctionRequest AddField(string fieldName, string stringValue)
+        {
+            form.AddField(fieldName, stringValue);
+            return this;
+        }
+
+        /// <summary>
+        /// Set request success callback
+        /// </summary>
+        public AzureFunctionRequest SetSuccessCallback(Action<AzureFunctionResponse> responseCallback)
+        {
+            successCB = responseCallback;
+            return this;
+        }
+
+        /// <summary>
+        /// Set request failure callback
+        /// </summary>
+        public AzureFunctionRequest SetFailureCallback(Action<AzureFunctionResponse> responseCallback)
+        {
+            failureCB = responseCallback;
+            return this;
+        }
+
+        /// <summary>
+        /// Send function request
+        /// </summary>
+        public void Send()
+        {
+            coroutineFunction = routineRunner.monoBehavior.StartCoroutine(ExecuteAzureWebFunction());
+            Debug.Log("AZURE FUNCTION SEND");
+        }
+
+        private IEnumerator ExecuteAzureWebFunction()
+        {
+            requestTimestamp = DateTime.UtcNow;
+            using (UnityWebRequest www = UnityWebRequest.Post(postURL, form))
+            {
+                yield return www.SendWebRequest();
+
+                responseTime = (long)((DateTime.UtcNow - requestTimestamp).TotalMilliseconds);
+                response.responseTime = responseTime;
+
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    if (failureCB != null)
+                    {
+                        response.isSuccess = false;
+                        failureCB(response);
+                    }
+                }
+                else if (successCB != null)
+                {
+                    response.isSuccess = true;
+                    successCB(response);
+                }
             }
         }
     }
 }
+
+
