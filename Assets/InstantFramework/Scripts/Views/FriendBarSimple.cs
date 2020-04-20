46af1016b1a8a4671de18fffc3c85fac59077ac0
Sami Atiq <sami.atiq@turbolabz.com>
2020-04-20 14:42:25 +0500

    Simpler friend bar Manage Blocked friends view

---
+++ Assets/InstantFramework/Scripts/Views/FriendBarSimple.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendBarSimple : MonoBehaviour
+{
+    public Image avatarImage;
+    public Image avatarBg;
+    public Image avatarIcon;
+    public GameObject premiumBorder;
+    public Text profileNameLabel;
+    public Text eloScoreLabel;
+    public Button stripButton;
+    public GameObject thinking;
+    public Image onlineStatus;
+    public Sprite online;
+    public Sprite offline;
+    public Sprite activeStatus;
+    public Button unblockButton;
+    public Text unblockButtonLabel;
+    public GameObject bottomAlphaBg;
+    public Mask maskObject;
+    public GameObject bgGlow;
+    public GameObject bgGlowLastStrip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
