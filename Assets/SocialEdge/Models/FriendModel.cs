dece90086666f4805c2fa0193d7f05d2699ca7bd
Omer Khalid Khan <omer.khalid@turbolabz.com>
2020-06-10 12:14:27 +0500

    FriendModel

---
+++ Assets/SocialEdge/Models/FriendModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static SocialEdge.Modules.PlayerModel;
+
+namespace SocialEdge.Modules
+{
+    public class FriendModel
+    {
+        public string playerId;
+        public string friendType;
+        public int gamesWon;
+        public int gamesLost;
+        public int gamesDrawn;
+        public long lastMatchTimestamp;
+        public PublicProfile publicProfile;
+    }
+
+}
