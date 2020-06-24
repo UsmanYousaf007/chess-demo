e3940893d85554c7ab79b6ed40019b2d9239885a
Noor Khawaja <noor.khawaja@turbolabz.com>
2020-06-24 16:12:43 +0500

    Tools added to be able to switch scenes quickly between Splash and Game

---
+++ Assets/Editor/ChessTools.cs
@@ -0,0 +1,36 @@
+﻿/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2016 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using TurboLabz.InstantFramework;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using TurboLabz.TLUtils;
+
+public class ChessTools : EditorWindow
+{
+    static string[] gameScenFiles = new string[] {
+            "Assets/InstantFramework/Scenes/Splash.unity",
+            "Assets/Game/Scenes/Game.unity"
+            };
+
+    [MenuItem("TLTools/Load Scene Splash")]
+    public static void SwitchToSplashScene()
+    {
+        LogUtil.Log("Switch to Splash scene", "yellow");
+        EditorSceneManager.OpenScene(gameScenFiles[0]);
+    }
+
+    [MenuItem("TLTools/Load Scene Game")]
+    public static void SwitchToGameScene()
+    {
+        LogUtil.Log("Switch to Game scene", "yellow");
+        EditorSceneManager.OpenScene(gameScenFiles[1]);
+    }
+}
