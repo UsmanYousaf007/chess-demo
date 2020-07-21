fd6758bd76ad19fb03ab437372a8be1e5c82d60d
Muhammad Ali Mustafa <ali.mustafa@turbolabz.com>
2020-07-21 14:00:26 +0500

    [wip] Video lessons script files.

---
+++ Assets/Game/Scripts/Lessons/Signals.cs
@@ -0,0 +1,14 @@
+﻿/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2018 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+using strange.extensions.signal.impl;
+using TurboLabz.InstantFramework;
+
+namespace TurboLabz.InstantGame
+{
+    // CPU LOBBY
+    //public class UpdateMenuViewSignal : Signal<LobbyVO> { }
+    public class ShowLessonVideoScreenSignal : Signal<string> { }
+}
---
+++ Assets/Game/Scripts/Lessons/Types/VideoLessonTopic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TurboLabz.InstantFramework;
+
+namespace TurboLabz.InstantGame
+{
+    [System.Serializable]
+    public class VideoLessonTopic
+    {
+        public string topicName;
+        public List<Video> videos;
+    }
+}
