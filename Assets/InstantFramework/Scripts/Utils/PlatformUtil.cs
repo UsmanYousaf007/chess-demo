8aa10749f2ddbc889fe16fc80a0d9f82dcb5311b
Omer Khalid Khan <omer.khalid@turbolabz.com>
2020-08-21 19:26:48 +0500

    Platform util files

---
+++ Assets/InstantFramework/Scripts/Utils/PlatformUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+namespace TurboLabz.TLUtils {
+    public static class PlatformUtil
+    {
+        public static Platform GetPlatform()
+        {
+#if UNITY_IOS
+            return Platform.ios;
+
+
+#elif UNITY_ANDROID
+            return Platform.Android;
+#endif
+        }
+
+        public static string AppendPlatform(this string str)
+        {
+            string suffix = "_" + GetPlatform().ToString().ToLower();
+            if (!str.EndsWith(suffix))
+                return str + suffix;
+            else
+                return str;
+        }
+
+        public static string RemovePlatfrom(this string str)
+        {
+            string substring = "_" + GetPlatform().ToString().ToLower();
+            if (str.EndsWith(substring))
+                return str.Substring(0, str.Length - substring.Length);
+            else
+                return str;
+        }
+    }
+}
