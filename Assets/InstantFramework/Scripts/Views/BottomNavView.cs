34de26d2d949b32f79a569127df5388af84bf3c1
Muhammad Ali Mustafa <ali.mustafa@turbolabz.com>
2020-08-26 15:00:25 +0500

    Resolved bottom view conflicts.

--- Assets/InstantFramework/Scripts/Views/BottomNavView.cs
+++ Assets/InstantFramework/Scripts/Views/BottomNavView.cs
@@ -23,38 +23,70 @@ namespace TurboLabz.InstantFramework
 {
     public class BottomNavView : View
     {
-        [Inject] public ILocalizationService localizationService { get; set; }
-
         public enum ButtonId
         {
             Home,
             Shop,
-            Friends
+            Friends,
+            Inventory
         }
 
         public ButtonId buttonId;
 
         public Text homeLabel;
         public Text friendsLabel;
+        public Text inventoryLabel;
+        public Text shopLabel;
 
         public Image homeIcon;
         public Image friendsIcon;
+        public Image inventoryIcon;
+        public Image shopIcon;
 
         public Button homeButton;
         public Button friendsButton;
+        public Button inventoryButton;
+        public Button shopButton;
+
+        public GameObject shopAlert;
+        public GameObject inventoryAlert;
 
         public Signal homeButtonClickedSignal = new Signal();
         public Signal friendsButtonClickedSignal = new Signal();
+        public Signal inventoryButtonClickedSignal = new Signal();
+        public Signal shopButtonClickedSignal = new Signal();
+
+        //Services
+        [Inject] public IAudioService audioService { get; set; }
+        [Inject] public ILocalizationService localizationService { get; set; }
+
+        //Models
+        [Inject] public IPreferencesModel preferencesModel { get; set; }
 
         public void Init()
         {
             homeLabel.text = localizationService.Get(LocalizationKey.NAV_HOME);
             friendsLabel.text = localizationService.Get(LocalizationKey.NAV_FRIENDS);
+            inventoryLabel.text = localizationService.Get(LocalizationKey.NAV_INVENTORY);
+            shopLabel.text = localizationService.Get(LocalizationKey.NAV_SHOP);
 
             homeButton.onClick.AddListener(HomeButtonClicked);
             friendsButton.onClick.AddListener(FriendsButtonClicked);
+            inventoryButton.onClick.AddListener(InventoryButtonClicked);
+            shopButton.onClick.AddListener(ShopButtonClicked);
 
             UpdateButtons();
+        }
+
+        private void OnEnable()
+        {
+            UpdateAlerts();
+        }
+
+        public void UpdateAlerts()
+        {
+            shopAlert.SetActive(!preferencesModel.shopTabVisited);
+            inventoryAlert.SetActive(!preferencesModel.inventoryTabVisited);
         }
 
         void UpdateButtons()
@@ -66,6 +98,14 @@ namespace TurboLabz.InstantFramework
             friendsButton.interactable = true;
             friendsIcon.color = Colors.WHITE_100;
             friendsLabel.color = Colors.WHITE_100;
+
+            inventoryButton.interactable = true;
+            inventoryIcon.color = Colors.WHITE_100;
+            inventoryLabel.color = Colors.WHITE_100;
+
+            shopButton.interactable = true;
+            shopIcon.color = Colors.WHITE_100;
+            shopLabel.color = Colors.WHITE_100;
 
             if (buttonId == ButtonId.Home)
             {
@@ -79,16 +119,44 @@ namespace TurboLabz.InstantFramework
                 friendsIcon.color = Colors.YELLOW;
                 friendsLabel.color = Colors.YELLOW;
             }
+            else if (buttonId == ButtonId.Inventory)
+            {
+                inventoryButton.interactable = false;
+                inventoryIcon.color = Colors.YELLOW;
+                inventoryLabel.color = Colors.YELLOW;
+            }
+            else if (buttonId == ButtonId.Shop)
+            {
+                shopButton.interactable = false;
+                shopIcon.color = Colors.YELLOW;
+                shopLabel.color = Colors.YELLOW;
+            }
         }
 
         void HomeButtonClicked()
         {
+            audioService.PlayStandardClick();
             homeButtonClickedSignal.Dispatch();
         }
 
         void FriendsButtonClicked()
         {
+            audioService.PlayStandardClick();
             friendsButtonClickedSignal.Dispatch();
+        }
+
+        void InventoryButtonClicked()
+        {
+            audioService.PlayStandardClick();
+            preferencesModel.inventoryTabVisited = true;
+            inventoryButtonClickedSignal.Dispatch();
+        }
+
+        void ShopButtonClicked()
+        {
+            audioService.PlayStandardClick();
+            preferencesModel.shopTabVisited = true;
+            shopButtonClickedSignal.Dispatch();
         }
     }
 }
