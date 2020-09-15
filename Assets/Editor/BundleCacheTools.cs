377c16e12dd7dad1538cdd087190e2d3105ba8af
Omer Khalid Khan <omer.khalid@turbolabz.com>
2020-09-15 16:35:40 +0500

    Tool for clearing asset bundles cache

---
+++ Assets/Editor/BundleCacheTools.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TurboLabz.InstantFramework;
+using UnityEditor;
+using UnityEngine;
+
+public partial class ChessTools
+{
+#if UNITY_ANDROID
+    private const string VERSION_CACHE_FILENAME = "downloadables_versioncache_andriod";
+#elif UNITY_IOS
+        private const string VERSION_CACHE_FILENAME = "downloadables_versioncache_ios";
+#endif
+    [MenuItem("TLTools/Asset Bundles/Clear Cache", false, 25)]
+    public static void ClearSkinCache()
+    {
+        EasySaveService easySaveService = new EasySaveService();
+        if (easySaveService.FileExists(VERSION_CACHE_FILENAME))
+        {
+            easySaveService.DeleteFile(VERSION_CACHE_FILENAME);
+        }
+
+        Caching.ClearCache();
+    }
+
+}
+
