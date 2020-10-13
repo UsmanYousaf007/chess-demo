ae639a960b18dac70a3f185850d1e9e4be630160
Maryam Rasheed <maryam.rasheed@turbolabz.com>
2020-10-13 12:29:35 +0500

    UPDATED: HUF: Country code

--- Assets/HUFEXT/CountryCode/Runtime/API/CountryCode.cs
+++ Assets/HUFEXT/CountryCode/Runtime/API/CountryCode.cs
@@ -4,9 +4,27 @@ namespace HUFEXT.CountryCode.Runtime.API
 {
     public class CountryCode
     {
-        [PublicAPI] public readonly string Language;
-        [PublicAPI] public readonly string Country;
-        [PublicAPI] public readonly string Region;
+        /// <summary>
+        /// System language code (ISO 639-1)
+        /// </summary>
+        [PublicAPI]
+        public readonly string Language;
+
+        /// <summary>
+        /// System set country code (ISO 3166-1)
+        /// </summary>
+        [PublicAPI]
+        public readonly string Country;
+
+        /// <summary>
+        /// IOS only region descriptor.
+        /// </summary>
+        /// <example>
+        /// <para>Assume IOS returns zh-Hans_HK as the language used.</para>
+        /// <para>It will be split to: Language: zh, Country: Hans, Region: HK.</para>
+        /// </example>
+        [PublicAPI]
+        public readonly string Region;
 
         public CountryCode(string language, string country, string region = "")
         {
@@ -21,22 +39,22 @@ namespace HUFEXT.CountryCode.Runtime.API
 
             switch ( result.Length )
             {
-                case 1: 
+                case 1:
                     Language = result[0].ToLower();
                     break;
-                
-                case 2: 
+
+                case 2:
                     Language = result[0].ToLower();
                     Country = result[1].ToLower();
                     break;
-                
-                case 3: 
+
+                case 3:
                     Language = result[0].ToLower();
                     Country = result[1].ToLower();
                     Region = result[2].ToLower();
                     break;
-                
-                default: 
+
+                default:
                     Language = "en";
                     Country = "us";
                     Region = string.Empty;
@@ -44,10 +62,24 @@ namespace HUFEXT.CountryCode.Runtime.API
             }
         }
 
+        /// <summary>
+        /// Gets formatted country code.
+        /// </summary>
+        /// <returns>Country code formatted as $"{<see cref="Language"/>}-{<see cref="Country"/>}".</returns>
         [PublicAPI]
         public string GetCountryCode()
         {
             return $"{Language}-{Country}";
+        }
+
+        /// <summary>
+        /// Converts localization details to dash separated format.
+        /// </summary>
+        /// <returns>Country code formatted as $"{<see cref="Language"/>}-{<see cref="Country"/>}".</returns>
+        [PublicAPI]
+        public override string ToString()
+        {
+            return GetCountryCode().ToLower().TrimEnd( '-' );
         }
     }
 }
--- Assets/HUFEXT/CountryCode/Runtime/API/HNativeCountryCode.cs
+++ Assets/HUFEXT/CountryCode/Runtime/API/HNativeCountryCode.cs
@@ -13,12 +13,16 @@ namespace HUFEXT.CountryCode.Runtime.API
     public static class HNativeCountryCode
     {
         static readonly ICountryCodeProvider countryCodeProvider;
-        
+
         static HNativeCountryCode()
         {
             countryCodeProvider = new PlatformCountryCodeProvider();
         }
-        
+
+        /// <summary>
+        /// Provides system localization information.
+        /// </summary>
+        /// <returns><seealso cref="CountryCode"/> object containing detailed localization information.</returns>
         [PublicAPI]
         public static CountryCode GetCountryCode()
         {
