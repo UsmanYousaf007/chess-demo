c02f89c1d5aa56a2c97dd9e3013da1269a4d6ad8
Omer Khalid Khan <omer.khalid@turbolabz.com>
2020-10-19 19:45:14 +0500

    Assemblies and prefab loading setup

--- Assets/Editor/ChessCreationTools.cs
+++ Assets/Editor/ChessCreationTools.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,18 +12,24 @@ public partial class ChessTools
     const string ASSET_PATH = "Assets/AssetBundles/BundleCreationAssets/";
     const string SKINS_PATH = "Assets/Game/Images/Skins/";
     const string ATLAS_PATH = "Assets/Game/Images/Atlases/";
+    const string PREFAB_PATH = "Assets/InstantFramework/Prefabs/";
 
     [MenuItem("TLTools/Create/Skin Bundles", false, 13)]
 
     public static void CreateSkinBundles()
     {
-        BuildTarget target = BuildTarget.iOS;
-        AssetBundleBuild[] bundleMap = CreateBundleMap(target);
-        BuildAllAssetBundles(target, target.ToString(), bundleMap);
+        
+        BuildTarget target = BuildTarget.Android;
+        AssetBundleBuild[] testbundlemap = CreateTestBundle();
+        BuildTestAssetBundle(target, target.ToString(), testbundlemap);
 
-        target = BuildTarget.Android;
-        bundleMap = CreateBundleMap(target);
-        BuildAllAssetBundles(target, target.ToString(), bundleMap);
+        //BuildTarget target = BuildTarget.iOS;
+        //AssetBundleBuild[] bundleMap = CreateBundleMap(target);
+        //BuildAllAssetBundles(target, target.ToString(), bundleMap);
+
+        //target = BuildTarget.Android;
+        //bundleMap = CreateBundleMap(target);
+        //BuildAllAssetBundles(target, target.ToString(), bundleMap);
     }
 
 
@@ -45,6 +52,34 @@ public partial class ChessTools
     }
 
     private static void BuildAllAssetBundles(BuildTarget target, string folder = null, AssetBundleBuild[] buildMap = null)
+    {
+        var assetBundleDirectory = "Assets/AssetBundles";
+        if (folder != null)
+            assetBundleDirectory = new StringBuilder(assetBundleDirectory).Append($"/{folder}").ToString();
+        if (!Directory.Exists(assetBundleDirectory))
+            Directory.CreateDirectory(assetBundleDirectory);
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, buildMap, BuildAssetBundleOptions.None, target);
+    }
+
+    private static AssetBundleBuild[] CreateTestBundle()
+    {
+        const string DLL_PATH = "Assets/";
+        string[] folders = Directory.GetDirectories(SKINS_PATH);
+        AssetBundleBuild[] bundleMap = new AssetBundleBuild[1];
+
+        var prefab = Directory.GetFiles(PREFAB_PATH, "TestExternalGameObj.prefab").FirstOrDefault();
+        var scriptdll = Directory.GetFiles(DLL_PATH, "ClassLibrary.bytes").FirstOrDefault();
+        bundleMap[0].assetBundleName = "DllBundle";
+            bundleMap[0].assetNames = new string[] {
+                scriptdll,
+                prefab
+            };
+
+        return bundleMap;
+    }
+
+    private static void BuildTestAssetBundle(BuildTarget target, string folder = null, AssetBundleBuild[] buildMap = null)
     {
         var assetBundleDirectory = "Assets/AssetBundles";
         if (folder != null)
---
+++ Assets/assemblyscript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+//using ClassLibrary;
+public class assemblyscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //testclass t = new testclass();
+        //Load texture from disk
+        //TextAsset dataAsset = (TextAsset)Resources.Load("ClassLibrary", typeof(TextAsset));
+        
+        //var assembly = System.Reflection.Assembly.Load(dataAsset.bytes);
+        //var type = assembly.GetType("ClassLibrary.testclass");
+        //var fields = type.GetFields();
+        //var types = assembly.GetTypes();
+
+
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "dllbundle"));
+        Debug.Log("Failed to load AssetBundle!");
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        var abc = myLoadedAssetBundle.GetAllAssetNames();
+        //var txt = myLoadedAssetBundle.LoadAsset("ClassLibrary", typeof(TextAsset)) as TextAsset;
+        var txt = myLoadedAssetBundle.LoadAsset<TextAsset>("assets/classlibrary.bytes");
+        // Load the TextAsset object
+
+
+        // Load the assembly and get a type (class) from it
+        var assembly = System.Reflection.Assembly.Load(txt.bytes);
+        //var type = assembly.GetType("ClassLibrary.testclass");
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("assets/instantframework/prefabs/testexternalgameobj.prefab");
+
+        //prefab.AddComponent(type);
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+
+        //type.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
