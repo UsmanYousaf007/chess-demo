6bd5b8e33b54b7868b8117247827f590ca4b1a53
Noor Khawaja <noor.khawaja@turbolabz.com>
2021-01-13 18:41:32 +0500

    Sound Effects update

--- Assets/InstantFramework/Scripts/Utils/RewardParticle.cs
+++ Assets/InstantFramework/Scripts/Utils/RewardParticle.cs
@@ -55,11 +55,11 @@ public class RewardParticle : View
     void PlaySequence()
     {
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DOMove(spreadPos, spreadTime));
         sequence.AppendCallback(() => playSFXSignal.Dispatch(spreadSFX));
+        sequence.Append(transform.DOMove(spreadPos, spreadTime));
         sequence.AppendInterval(spreadAirTime);
-        sequence.Append(transform.DOMove(targetObject.transform.position, flyTime));
         sequence.AppendCallback(() => playSFXSignal.Dispatch(travelSFX));
+        sequence.Append(transform.DOMove(targetObject.transform.position, flyTime));
         sequence.AppendCallback(EndReached);
         sequence.PlayForward();
     }
