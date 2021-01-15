a1bf0d3529178f9c62fdd42ee99e10e68c415ad3
Noor Khawaja <noor.khawaja@turbolabz.com>
2021-01-15 18:04:54 +0500

    Reward particle emitter mediator to access audio service

---
+++ Assets/InstantFramework/Scripts/Utils/RewardParticleEmitterMediator.cs
@@ -0,0 +1,35 @@
+﻿
+/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Turbo Labz 2021 - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
+using UnityEngine;
+using DG.Tweening;
+using strange.extensions.mediation.impl;
+using strange.extensions.signal.impl;
+
+using System.Collections;
+using System.Collections.Generic;
+using strange.extensions.mediation.impl;
+using TurboLabz.InstantFramework;
+using UnityEngine;
+
+public class RewardParticleEmitterMediator : Mediator
+{
+    //View Injection
+    [Inject] public RewardParticleEmitter view { get; set; }
+
+    //Services
+    [Inject] public IAudioService audioService { get; set; }
+
+    public override void OnRegister()
+    {
+        view.playSFXSignal.AddListener(OnPlaySFX);
+    }
+
+    private void OnPlaySFX(AudioClip clip)
+    {
+        audioService.Play(clip);
+    }
+}
