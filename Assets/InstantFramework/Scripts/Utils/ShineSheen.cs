84c8ff2cf4e9729d2201e6b0157af44a0887e533
Noor Khawaja <noor.khawaja@turbolabz.com>
2021-01-19 03:17:25 +0500

    Animated shine on Play button

---
+++ Assets/InstantFramework/Scripts/Utils/ShineSheen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShineSheen : MonoBehaviour
+{
+    public Transform shine;
+
+    private float offset = 400;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animate();   
+    }
+
+    private void Animate()
+    {
+        shine.DOLocalMoveX(offset, 2.0f).SetEase(Ease.Linear).SetDelay(Random.Range(5.0f, 7.0f)).OnComplete(() =>
+            {
+                shine.DOLocalMoveX(-offset, 0);
+                Animate();
+            });
+    }
+}
