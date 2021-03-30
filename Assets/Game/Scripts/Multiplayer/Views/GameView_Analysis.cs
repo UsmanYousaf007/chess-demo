5d0cf9582f392c3fa6764cf95d0f01c4ce5ff119
Sami Atiq <sami.atiq@turbolabz.com>
2021-03-30 20:17:46 +0500

    ADDED: best move and strength for analysied move in view

--- Assets/Game/Scripts/Multiplayer/Views/GameView_Analysis.cs
+++ Assets/Game/Scripts/Multiplayer/Views/GameView_Analysis.cs
@@ -23,6 +23,10 @@ namespace TurboLabz.Multiplayer
         public Text analysisStrengthLabel;
         public Image analysisStrengthFiller;
         public Image analysisStrengthFillerBg;
+        public Transform analysisPlayerMovePivotHolder;
+        public Transform analysisPlayerMoveLeftPivot;
+        public Transform analysisPlayerMoveRightPivot;
+        public Image analysisMoveQuality;
 
         public Signal<List<MoveAnalysis>> onAnalysiedMoveSelectedSignal = new Signal<List<MoveAnalysis>>();
 
@@ -37,6 +41,10 @@ namespace TurboLabz.Multiplayer
         public void InitAnalysis()
         {
             pickerSrollRect.onSelectItem.AddListener(OnMoveSelected);
+
+            var originalScale = analysisPlayerMovePivotHolder.localScale;
+            var vectorToScale = new Vector3(originalScale.x * scaleUniform, originalScale.y * scaleUniform, 1);
+            analysisPlayerMovePivotHolder.localScale = vectorToScale;
         }
 
         public void UpdateAnalysisView(List<MoveAnalysis> moves)
@@ -48,6 +56,10 @@ namespace TurboLabz.Multiplayer
             playerInfoPanel.SetActive(false);
             analysisPanel.SetActive(true);
             uiBlocker.SetActive(false);
+            HideOpponentToIndicator();
+            HideOpponentFromIndicator();
+            HidePlayerToIndicator();
+            HidePlayerFromIndicator();
         }
 
         private void ClearMovesList()
@@ -65,13 +77,16 @@ namespace TurboLabz.Multiplayer
             {
                 var moveView = Instantiate(analysisMoveView, movesContainer);
                 var moveVO = moveView.GetComponent<AnalysisMoveView>();
-
-                moveVO.SetupMove($"{i}.", move.playerMove.ToShortString(),
-                    move.moveQuality == MoveQuality.BLUNDER ? moveAnalysisBlunder :
-                    move.moveQuality == MoveQuality.MISTAKE ? moveAnalysisMistake :
-                    move.moveQuality == MoveQuality.PERFECT ? moveAnalysisPerfect : null);
+                moveVO.SetupMove($"{i}.", move.playerMove.ToShortString(), GetMoveQualitySprite(move.moveQuality));
                 i++;
             }
+        }
+
+        private Sprite GetMoveQualitySprite(MoveQuality quality)
+        {
+            return quality == MoveQuality.BLUNDER ? moveAnalysisBlunder :
+                   quality == MoveQuality.MISTAKE ? moveAnalysisMistake :
+                   quality == MoveQuality.PERFECT ? moveAnalysisPerfect : null;
         }
 
         private void OnMoveSelected(GameObject obj)
@@ -82,6 +97,7 @@ namespace TurboLabz.Multiplayer
             var selectedMoves = GetSelectedMoves(moveIndex);
 
             onAnalysiedMoveSelectedSignal.Dispatch(selectedMoves);
+            OnAnalysisBoardUpdated(moveIndex);
         }
 
         private List<MoveAnalysis> GetSelectedMoves(int moveIndex)
@@ -94,6 +110,68 @@ namespace TurboLabz.Multiplayer
             }
 
             return selectedMoves;
+        }
+
+        private void OnAnalysisBoardUpdated(int moveIndex)
+        {
+            var analysiedMove = analysiedMoves[moveIndex - 1];
+            var mainCamera = Camera.main;
+
+            var playerMoveFromIndex = RankFileMap.Map[analysiedMove.playerMove.from.rank, analysiedMove.playerMove.from.file];
+            var playerMoveToIndex = RankFileMap.Map[analysiedMove.playerMove.to.rank, analysiedMove.playerMove.to.file];
+            var playerMoveFromPosition = chessboardSquares[playerMoveFromIndex].position;
+            var playerMoveToPosition = chessboardSquares[playerMoveToIndex].position;
+
+            var bestMoveFromIndex = RankFileMap.Map[analysiedMove.bestMove.from.rank, analysiedMove.bestMove.from.file];
+            var bestMoveToIndex = RankFileMap.Map[analysiedMove.bestMove.to.rank, analysiedMove.bestMove.to.file];
+            var bestMoveFromPosition = chessboardSquares[bestMoveFromIndex].position;
+            var bestMoveToPosition = chessboardSquares[bestMoveToIndex].position;
+
+            var angle = Mathf.Atan2(bestMoveFromPosition.y - bestMoveToPosition.y, bestMoveFromPosition.x - bestMoveToPosition.x) * Mathf.Rad2Deg;
+            var bestMoveToScreenPosition = mainCamera.WorldToScreenPoint(bestMoveToPosition);
+            var showStrengthPanelOnRight = playerMoveToPosition.x < 0;
+            var showStrengthPanel = analysiedMove.moveQuality != MoveQuality.PERFECT;
+
+            //Player move indicators
+            hindsightFromIndicator.transform.position = playerMoveFromPosition;
+            hindsightToIndicator.transform.position = playerMoveToPosition;
+            hindsightFromIndicator.SetActive(true);
+            hindsightToIndicator.SetActive(true);
+
+            //Placing and rotating arrow head
+            analysisArrowHead.rectTransform.position = bestMoveToScreenPosition;
+            analysisArrowHead.transform.localEulerAngles = new Vector3(0, 0, angle);
+
+            //Drawing the line from square to arrow head
+            analysisLine.Draw(bestMoveFromPosition, mainCamera.ScreenToWorldPoint(analysisLineEndPivot.position));
+
+            //Placing pivot holder for strength and move quality
+            analysisPlayerMovePivotHolder.position = playerMoveToPosition;
+            var leftPivotPosition = mainCamera.WorldToScreenPoint(analysisPlayerMoveLeftPivot.position);
+            var rightPivotPosition = mainCamera.WorldToScreenPoint(analysisPlayerMoveRightPivot.position);
+
+            //Setting move quality sprite
+            analysisMoveQuality.sprite = GetMoveQualitySprite(analysiedMove.moveQuality);
+            analysisMoveQuality.enabled = analysiedMove.moveQuality != MoveQuality.NORMAL;
+
+            //Setting strength value
+            analysisStrengthLabel.text = $"{Mathf.RoundToInt(analysiedMove.strength * 100)}%";
+            analysisStrengthFiller.fillAmount = analysiedMove.strength;
+
+            //Placing strength panel and quality icon
+            FlipAnalysisStrengthPanel(showStrengthPanelOnRight ? 1 : -1);
+            analysisStrengthPanel.transform.position = showStrengthPanelOnRight ? rightPivotPosition : leftPivotPosition;
+            analysisMoveQuality.transform.position = showStrengthPanelOnRight && showStrengthPanel ? leftPivotPosition : rightPivotPosition;
+            analysisStrengthPanel.SetActive(showStrengthPanel);
+        }
+
+        private void FlipAnalysisStrengthPanel(float scale)
+        {
+            var flipVector = new Vector3(scale, 1, 1);
+            analysisStrengthPanel.transform.localScale = 
+                analysisStrengthFiller.transform.localScale = 
+                analysisStrengthFillerBg.transform.localScale = 
+                analysisStrengthLabel.transform.localScale = flipVector;
         }
     }
 }
