bb3fcb80b6e711ec37184a909010f5be0731ffac
Maryam Rasheed <maryam.rasheed@turbolabz.com>
2021-03-31 15:37:51 +0500

    Lean Tween removed

--- Assets/Game/Scripts/Multiplayer/Views/GameView_Analysis.cs
+++ Assets/Game/Scripts/Multiplayer/Views/GameView_Analysis.cs
@@ -56,6 +56,7 @@ namespace TurboLabz.Multiplayer
             playerInfoPanel.SetActive(false);
             analysisPanel.SetActive(true);
             uiBlocker.SetActive(false);
+            SetBlurBg(false);
             HideOpponentToIndicator();
             HideOpponentFromIndicator();
             HidePlayerToIndicator();
