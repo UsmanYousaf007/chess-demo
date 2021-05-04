36e18c7cec3888320a4eb6fe9095db2d4013f534
Omer Khalid Khan <omer.khalid@turbolabz.com>
2021-05-04 17:43:15 +0500

    REFACTOR: Maintenance mode

--- Assets/InstantFramework/Scripts/Views/MaintenanceMediator.cs
+++ Assets/InstantFramework/Scripts/Views/MaintenanceMediator.cs
@@ -1,4 +1,5 @@
-﻿using strange.extensions.mediation.impl;
+﻿using System;
+using strange.extensions.mediation.impl;
 
 namespace TurboLabz.InstantFramework
 {
@@ -6,10 +7,12 @@ namespace TurboLabz.InstantFramework
     {
         // View injection
         [Inject] public MaintenanceView view { get; set; }
+        [Inject] public ISchedulerService schedulerService { get; set; }
 
         public override void OnRegister()
         {
             view.Init();
+            view.schedulerSubscription.AddListener(OnSchedulerSubscriptionToggle);
         }
 
         [ListensTo(typeof(NavigatorShowViewSignal))]
@@ -46,6 +49,18 @@ namespace TurboLabz.InstantFramework
                 view.HideMaintenanceWarning();
             }
 
+        }
+
+        private void OnSchedulerSubscriptionToggle(bool subscribe)
+        {
+            if (subscribe)
+            {
+                schedulerService.Subscribe(view.SchedulerCallback);
+            }
+            else
+            {
+                schedulerService.UnSubscribe(view.SchedulerCallback);
+            }
         }
     }
 }
